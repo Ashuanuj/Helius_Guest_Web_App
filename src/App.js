6be78d7c0c65960445d757622431cc4fb7fd4b2a
Ashuanuj <ashutoshchoubeyanuj@gmail.com>
2019-09-27 17:29:52 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -8,38 +8,38 @@ import React from 'react';
 import componentQueries from 'react-component-queries';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './styles/myStyles.scss';
-import { CSSTransition } from 'react-transition-group'
+// import { CSSTransition } from 'react-transition-group'
 
-import DashboardPage from './pages/DashboardPage';
-import ServicesPage from './pages/ServicesPage';
-import MainSubcategoryPage from './pages/SubcategoryPages/MainSubcategoryPage';
-import CheckoutPage from './pages/CheckoutPage';
-import WakeUp from './pages/WakeUp';
-import FrontOffice from './pages/FrontOffice';
-import RequestMain from './pages/RequestPages/RequestMain';
+// import DashboardPage from './pages/DashboardPage';
+// import ServicesPage from './pages/ServicesPage';
+// import MainSubcategoryPage from './pages/SubcategoryPages/MainSubcategoryPage';
+// import CheckoutPage from './pages/CheckoutPage';
+// import WakeUp from './pages/WakeUp';
+// import FrontOffice from './pages/FrontOffice';
+// import RequestMain from './pages/RequestPages/RequestMain';
 
-// const DashboardPage = React.lazy(() => import('pages/DashboardPage'));
-// const ServicesPage = React.lazy(() => import('pages/ServicesPage'));
-// const MainSubcategoryPage = React.lazy(() => import('pages/SubcategoryPages/MainSubcategoryPage'));
-// const CheckoutPage = React.lazy(() => import('pages/CheckoutPage'));
-// const WakeUp = React.lazy(() => import('pages/WakeUp'));
-// const FrontOffice = React.lazy(() => import('pages/FrontOffice'));
-// const RequestMain =  React.lazy(() => import('pages/RequestPages/RequestMain'));
+const DashboardPage = React.lazy(() => import('pages/DashboardPage'));
+const ServicesPage = React.lazy(() => import('pages/ServicesPage'));
+const MainSubcategoryPage = React.lazy(() => import('pages/SubcategoryPages/MainSubcategoryPage'));
+const CheckoutPage = React.lazy(() => import('pages/CheckoutPage'));
+const WakeUp = React.lazy(() => import('pages/WakeUp'));
+const FrontOffice = React.lazy(() => import('pages/FrontOffice'));
+const RequestMain =  React.lazy(() => import('pages/RequestPages/RequestMain'));
 
 const getBasename = () => {
   return `/${process.env.PUBLIC_URL.split('/').pop()}`;
 };
 
 
-const routes = [
-  { path:"/dashboard", Component:DashboardPage },
-  { path:"/services", Component:ServicesPage },
-  { path:"/subcategorypages/mainsubcategorypage" ,Component:MainSubcategoryPage },
-  { path:"/checkout",Component:CheckoutPage },
-  { path:"/wakeup" ,Component:WakeUp},
-  { path:"/frontoffice",Component:FrontOffice},
-  { path:"/requestmain" ,Component:RequestMain},
-]
+// const routes = [
+//   { path:"/dashboard", Component:DashboardPage },
+//   { path:"/services", Component:ServicesPage },
+//   { path:"/subcategorypages/mainsubcategorypage" ,Component:MainSubcategoryPage },
+//   { path:"/checkout",Component:CheckoutPage },
+//   { path:"/wakeup" ,Component:WakeUp},
+//   { path:"/frontoffice",Component:FrontOffice},
+//   { path:"/requestmain" ,Component:RequestMain},
+// ]
 
 class App extends React.Component {
   render() {
@@ -67,7 +67,7 @@ class App extends React.Component {
 
             <MainLayout breakpoint={this.props.breakpoint}>
                <React.Suspense fallback={<PageSpinner />}>
-              {routes.map(({ path, Component }) => (
+              {/* {routes.map(({ path, Component }) => (
                   <Route key={path} exact path={path}>
                     {({ match }) => (
                       <CSSTransition
@@ -82,14 +82,14 @@ class App extends React.Component {
                       </CSSTransition>
                     )}
                   </Route>
-                ))}
-                {/* <Route exact path="/dashboard" component={DashboardPage} />
+                ))} */}
+                <Route exact path="/dashboard" component={DashboardPage} />
                 <Route exact path="/services" component={ServicesPage} />
                 <Route exact path="/subcategorypages/mainsubcategorypage" component={MainSubcategoryPage} />
                 <Route exact path="/checkout" component={CheckoutPage} />
                 <Route exact path="/wakeup" component={WakeUp} />
                 <Route exact path="/frontoffice" component={FrontOffice} />
-                <Route exact path="/requestmain" component={RequestMain} /> */}
+                <Route exact path="/requestmain" component={RequestMain} />
               </React.Suspense>
             </MainLayout>
             <Redirect to="/" />
